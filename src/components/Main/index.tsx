5a50a5d0c91303e0c00d4f6a9f6998078fac2cdb
Wilson Franca <wilson.franca.92@gmail.com>
2021-12-27 18:55:05 -0800

    Styled sample Main component with styled components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles';
+
 export const Main = () => {
   return (
-    <main>
-      <h1>Advanced React</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/assets/images/logo.svg"
+        alt="Image of an atom and Advanced React written side by side"
+      />
+      <S.Heading>Advanced React</S.Heading>
+      <S.SubHeading>
+        TypeScript, React, NextJS, and Styled Components
+      </S.SubHeading>
+      <S.Illustration
+        src="/assets/images/hero-illustration.svg"
+        alt="A developer programming in front of a computer screen"
+      />
+    </S.Wrapper>
   );
 };
